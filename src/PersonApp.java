97394c0ad68dcbea2b6a1aec38a6994591a915af
Abdul Rozak <arozak25@gmail.com>
2021-01-09 20:46:47 +0700

    [Object] create PersonApp class and create some object

---
+++ src/PersonApp.java
@@ -0,0 +1,18 @@
+/**
+ * @author Abdul Rozak
+ * @since 1/9/21
+ */
+public class PersonApp {
+
+    public static void main(String[] args){
+
+        var person1 = new Person();
+        Person person2 = new Person();
+        Person person3;
+        person3 = new Person();
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+    }
+}
