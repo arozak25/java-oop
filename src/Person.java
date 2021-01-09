08d0fb919d365d98055e5bc99fd02f3008609af5
Abdul Rozak <arozak25@gmail.com>
2021-01-09 20:53:34 +0700

    [Field] create some field in Person class and manipulate them

--- src/Person.java
+++ src/Person.java
@@ -3,4 +3,8 @@
  * @since 1/9/21
  */
 class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
 }
+
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -7,12 +7,15 @@ public class PersonApp {
     public static void main(String[] args){
 
         var person1 = new Person();
+        person1.name = "Rozak";
+        person1.address = "Bogor";
+//        person1.country = "Amerika" //ERROR
         Person person2 = new Person();
         Person person3;
         person3 = new Person();
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
     }
 }
