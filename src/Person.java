6ef73a238e418a158f8a167f5aa0cc86f80f35a8
Abdul Rozak <arozak25@gmail.com>
2021-01-15 20:16:16 +0700

    [this Keyword] add this keyword to shadowed variable

--- src/Person.java
+++ src/Person.java
@@ -7,20 +7,20 @@ class Person {
     String address;
     final String country = "Indonesia";
 
-    Person(String paramName, String paramAddress){
-        name = paramName;
-        address = paramAddress;
+    Person(String name, String address){
+        this.name = name;
+        this.address = address;
     }
 
-    Person(String paramName){
-        this(paramName, null);
+    Person(String name){
+        this(name, null);
     }
 
     Person(){
     }
 
-    void sayHello(String paramName){
-        System.out.println("Hello " + paramName + "!, my name is " + name);
+    void sayHello(String name){
+        System.out.println("Hello " + name + "!, my name is " + this.name);
     }
 }
 
