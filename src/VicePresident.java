f54cb6b9500f392ab77728946a01293cc6b8896b
Abdul Rozak <arozak25@gmail.com>
2021-01-15 20:28:34 +0700

    [Method Overriding] override hello method on VicePresident

--- src/VicePresident.java
+++ src/VicePresident.java
@@ -3,4 +3,8 @@
  * @since 1/15/21
  */
 class VicePresident extends Manager {
+
+    void sayHello(String name){
+        System.out.println("Hello " + name + "!, my name is vp " + this.name);
+    }
 }
