ae68bbd9f60459474363add33fb074c1066c255b
Abdul Rozak <arozak25@gmail.com>
2021-01-17 21:36:48 +0700

    [Package] create package

---
+++ src/belajar/ojak/data/Product.java
@@ -0,0 +1,15 @@
+package belajar.ojak.data;
+
+/**
+ * @author Abdul Rozak
+ * @since 1/17/21
+ */
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+}
