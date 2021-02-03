0459f141df39edfebb9aff2545b1587fd2b37275
Abdul Rozak <arozak25@gmail.com>
2021-02-04 02:23:25 +0700

    [Final Class] add SocialMedia class

---
+++ src/belajar/ojak/data/SocialMedia.java
@@ -0,0 +1,17 @@
+package belajar.ojak.data;
+
+/**
+ * @author Abdul Rozak
+ * @since 2/4/21
+ */
+class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+//class FakeFacebook extends Facebook { // error: cannot inherit from final
+//
+//}
